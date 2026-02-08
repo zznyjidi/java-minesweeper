d548a945789645ab0b289c1167763cf1e5475688
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2026-02-08 10:43:21 -0500

    add: testing interface

--- app/src/app/main/Main.java
+++ app/src/app/main/Main.java
@@ -5,18 +5,32 @@ import minesweeper.Space;
 
 public class Main {
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper(10, 10, 99);
+        Minesweeper minesweeper = new Minesweeper(10, 10, 3);
 
-        int initX = 0;
-        int initY = 0;
+        int initX = 3;
+        int initY = 5;
 
-        minesweeper.generateGrid(initX, initY);
+        minesweeper.flip(initX, initY);
         Space[][] grid = minesweeper.getGrid();
         for (int y = 0; y < grid.length; y++) {
             for (int x = 0; x < grid[0].length; x++) {
                 IO.print((grid[y][x].isMine() ? "X" : "O") + (x == initX && y == initY ? "<" : " "));
             }
             IO.println();
+        }
+
+        while (true) {
+            for (int iy = 0; iy < grid.length; iy++) {
+                for (int ix = 0; ix < grid[0].length; ix++) {
+                    IO.print(grid[iy][ix].toString() + " ");
+                }
+                IO.println();
+            }
+
+            int x = Integer.parseInt(IO.readln("x"));
+            int y = Integer.parseInt(IO.readln("y"));
+
+            IO.println(minesweeper.flip(x, y));
         }
     }
 }
