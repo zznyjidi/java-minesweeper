ab04879f08e4901ebe692dcedd46ebdf2dbc543d
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2026-02-08 11:45:07 -0500

    feat: basic interface

--- app/src/app/main/Main.java
+++ app/src/app/main/Main.java
@@ -1,36 +1,80 @@
 package main;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import minesweeper.Minesweeper;
 import minesweeper.Space;
 
 public class Main {
+    static Pattern commandPattern = Pattern.compile("(flag|flip) (\\d+) (\\d+)");
+    static boolean running = true;
+
     public static void main(String[] args) {
         Minesweeper minesweeper = new Minesweeper(10, 10, 3);
 
-        int initX = 3;
-        int initY = 5;
+        while (running) {
+            printGrid(minesweeper, false);
 
-        minesweeper.flip(initX, initY);
+            String command = IO.readln("> ");
+            Matcher matcher = commandPattern.matcher(command);
+
+            if (matcher.find()) {
+                String cmd = matcher.group(1);
+                int x = Integer.parseInt(matcher.group(2));
+                int y = Integer.parseInt(matcher.group(3));
+
+                switch (cmd) {
+                    case "flag" -> minesweeper.flag(x, y);
+                    case "flip" -> {
+                        switch (minesweeper.flip(x, y)) {
+                            case Minesweeper.FLIP_BOMB -> {
+                                printGrid(minesweeper, true);
+                                IO.println("YOU LOSE! ");
+                                running = false;
+                            }
+                            case Minesweeper.FLIP_WIN -> {
+                                printGrid(minesweeper, true);
+                                IO.println("YOU WIN! ");
+                                running = false;
+                            }
+                            case Minesweeper.FLIP_ALREADY_FLIPPED -> {
+                                IO.println("BLOCK ALREADY FLIPPED. ");
+                            }
+                            case Minesweeper.FLIP_FLAGGED -> {
+                                IO.println("BLOCK FLAGGED. ");
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    public static void printGrid(Minesweeper minesweeper, boolean showMine) {
         Space[][] grid = minesweeper.getGrid();
-        for (int y = 0; y < grid.length; y++) {
-            for (int x = 0; x < grid[0].length; x++) {
-                IO.print((grid[y][x].isMine() ? "X" : "O") + (x == initX && y == initY ? "<" : " "));
+
+        // Grid X Mark
+        IO.print("   |");
+        for (int i = 0; i < grid[0].length; i++)
+            IO.print(String.format("%2d", i));
+        IO.println();
+
+        // Line
+        IO.println("---+-" + "--".repeat(grid[0].length));
+
+        // Grid
+        for (int iy = 0; iy < grid.length; iy++) {
+            // Grid Y Mark
+            IO.print(String.format("%2d | ", iy));
+
+            // Grid Row
+            for (int ix = 0; ix < grid[0].length; ix++) {
+                Space space = grid[iy][ix];
+                IO.print(space.toString() + (showMine ? (space.isMine() ? "<" : " ") : " "));
             }
             IO.println();
-        }
-
-        while (true) {
-            for (int iy = 0; iy < grid.length; iy++) {
-                for (int ix = 0; ix < grid[0].length; ix++) {
-                    IO.print(grid[iy][ix].toString() + " ");
-                }
-                IO.println();
-            }
-
-            int x = Integer.parseInt(IO.readln("x"));
-            int y = Integer.parseInt(IO.readln("y"));
-
-            IO.println(minesweeper.flip(x, y));
         }
     }
 }
